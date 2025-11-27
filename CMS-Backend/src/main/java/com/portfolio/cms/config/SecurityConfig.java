4d3b725c43981da8af78b7a27267242be9df0a9d
sathya <2300033302@kluniversity.in>
2025-11-27 21:50:11 +0530

    v4.0

--- CMS-Backend/src/main/java/com/portfolio/cms/config/SecurityConfig.java
+++ CMS-Backend/src/main/java/com/portfolio/cms/config/SecurityConfig.java
@@ -55,7 +55,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+       configuration.setAllowedOrigins(
+        Arrays.asList("http://localhost:5173", "http://localhost:3000")
+);
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
